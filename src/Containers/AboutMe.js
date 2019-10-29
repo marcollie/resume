20ff2e83d6d48beec77de8373437c85f3c4bfce9
marcollie <imwhoa@naver.com>
2019-10-30 04:02:21 +0900

    Skill toggle func

--- src/Containers/AboutMe.js
+++ src/Containers/AboutMe.js
@@ -1,12 +1,55 @@
-import React from "react";
+import React from 'react';
+import styled from 'styled-components'
+
+const Wrap = styled.div`
+  margin:  2rem 7rem 2rem 7rem;
+`
+
+const Skill = styled.div`
+  display:inline;
+`
+const H4 = styled.h4`
+  cursor: pointer;
+  display:inline;
+`
+
+
+function mouseClick () {
+  const skId = document.getElementById("skills");
+  const barId = document.getElementById("sBar")
+  if(skId.style.display === "none") {
+    skId.style.display = "inline";
+    barId.innerHTML = " > "
+  } else {
+    skId.style.display = "none";
+    barId.innerHTML = " < "
+  }
+}
 
 const AboutMe = () => {
   return (
 
-      <div>
-        HI
-      </div>
-
+      <Wrap>
+        <p>
+        이유상<br />
+        1989. 12. 12<br />
+        <br />
+        서현고등학교 졸업 (2008.02)<br />
+        선문대학교 스페인중남미어학과 졸업 (2019.02)<br />
+        <br />
+        이젠아카데미컴퓨터학원<br /> 
+        - 스마트미디어응용 디자인콘텐츠 개발과정 수료 (2018.11)
+        </p>
+        <br />
+        <Skill>
+          <H4 onClick={mouseClick}>Skill 
+            <h4 id="sBar" style={{display:"inline"}}> &lt; </h4>
+          </H4>
+          <p id="skills" style={{display:"none"}}>
+            PhotoShop&ensp;  Illustrator&ensp;  HTML5&ensp;  CSS&ensp;  JavaScript&ensp;  React
+          </p>
+        </Skill>
+      </Wrap>
   )
 }
 
